219234407ad42c7e4abeb22030026645b01d3130
Benjamin W. Gafford <gaffordb@cs.grinnell.edu>
2017-11-02 20:23:35 -0500

    Add base stats

--- LootGenerator/LootGenerator.java
+++ LootGenerator/LootGenerator.java
@@ -6,11 +6,13 @@ import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.TreeMap;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class LootGenerator {
 	public static void main(String[] args) throws FileNotFoundException {
 		File monstersFile = new File("data/large/monstats.txt");
 		File armorFile = new File("data/large/TreasureClassEx.txt");
+		File armorFileStats = new File("data/large/armor.txt");
 		Monster m = pickMonster(monstersFile);
 		System.out.println("Fighting " + m.name);
 		System.out.println("You have slain " + m.name + "!");
@@ -18,6 +20,8 @@ public class LootGenerator {
 		System.out.println();
 		String loot = fetchTreasureClass(m, armorFile);
 		System.out.println(loot);
+		int baseS = generateBaseStats(loot, armorFileStats);
+		System.out.println("Defense: " + baseS);
 		
 		
 	}
@@ -54,5 +58,22 @@ public class LootGenerator {
 			curKey = armorMap.get(curKey)[index];
 		}
 		return curKey;
+	}
+	
+	public static int generateBaseStats(String loot, File f) throws FileNotFoundException {
+		Scanner s = new Scanner(f);
+		
+		while(s.hasNext()) {
+			String curLine = s.nextLine();
+			String[] stats = curLine.split("\t");
+			if(stats[0].equals(loot)) {
+				int randomNum = ThreadLocalRandom.current().
+						nextInt(Integer.parseInt(stats[1]), Integer.parseInt(stats[2]));
+				s.close();
+				return randomNum;
+			}
+		}
+		s.close();
+		throw new IllegalArgumentException();
 	}
 }
